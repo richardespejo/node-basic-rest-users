6242d249e0344b31436c7fe02e281c150a78014e
Richard Espejo <richardespejo@hotmail.es>
2025-08-04 18:33:55 +0200

    se añade buscador

--- models/server.js
+++ models/server.js
@@ -9,6 +9,7 @@ class Server {
 
         this.paths = {
             auth: '/api/auth',
+            buscar: '/api/buscar',
             usuarios: '/api/usuarios',
             categorias: '/api/categorias',
             productos: '/api/productos'
@@ -41,6 +42,7 @@ class Server {
 
     routes(){
         this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
